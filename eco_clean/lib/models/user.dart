4c5ff0e72cf4d139266dd27571d9f6af6d39d561
PrathibhaSithu <sithuminiprathiba@gmail.com>
2024-03-17 23:40:25 +0530

    Model class user

--- eco_clean/lib/models/user.dart
+++ eco_clean/lib/models/user.dart
@@ -15,19 +15,12 @@ class Users {
         uuid = '';
 
   Users.fromMap(Map<String, dynamic> data)
-      : displayName = '',
-        email = '',
-        password = '',
-        phone = '',
-        role = '',
-        uuid = '' {
-    displayName = data['displayName'];
-    email = data['email'];
-    password = data['password'];
-    phone = data['phone'];
-    role = data['role'];
-    uuid = data['uuid'];
-  }
+      : displayName = data['displayName'],
+        email = data['email'],
+        password = data['password'],
+        phone = data['phone'],
+        role = data['role'],
+        uuid = data['uuid'];
 
   Map<String, dynamic> toMap() {
     return {
@@ -40,3 +33,35 @@ class Users {
     };
   }
 }
+
+
+// class Users {
+//   String displayName;
+//   String email;
+//   String password;
+//   String phone;
+//   String role;
+//   String uuid;
+
+//   Users(); // default constructor
+
+//   Users.fromMap(Map<String, dynamic> data) {
+//     displayName = data['displayName'];
+//     email = data['email'];
+//     password = data['password'];
+//     phone = data['phone'];
+//     role = data['role'];
+//     uuid = data['uuid'];
+//   }
+
+//   Map<String, dynamic> toMap() {
+//     return {
+//       'displayName': displayName,
+//       'email': email,
+//       'password': password,
+//       'phone': phone,
+//       'role': role,
+//       'uuid': uuid,
+//     };
+//   }
+// }
