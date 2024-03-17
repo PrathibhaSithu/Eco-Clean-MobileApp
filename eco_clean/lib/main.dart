b27d649b9153b1a60f79333d9aa8f54826f572b7
PrathibhaSithu <sithuminiprathiba@gmail.com>
2024-03-17 11:53:24 +0530

    Update main.dart

---
+++ eco_clean/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
