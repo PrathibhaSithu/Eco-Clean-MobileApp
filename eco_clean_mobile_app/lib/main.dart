1bbda46c0991c2d875029896589ad2e7d79ee2a5
PrathibhaSithu <sithuminiprathiba@gmail.com>
2024-03-20 11:22:20 +0530

    Banner false

--- eco_clean_mobile_app/lib/main.dart
+++ eco_clean_mobile_app/lib/main.dart
@@ -35,6 +35,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      debugShowCheckedModeBanner: false,
       home: Wrapper(),
       // home:  Scaffold(
       //   appBar: AppBar(
