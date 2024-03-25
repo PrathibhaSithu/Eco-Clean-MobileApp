d7cd77e9bfaa7cbaf37e0e269a7cfba27a88b0fc
IT21700156 <it21700156@my.sliit.lk>
2024-03-25 22:01:10 +0530

    akila-update-firebase

--- eco_clean_mobile_app/lib/main.dart
+++ eco_clean_mobile_app/lib/main.dart
@@ -1,10 +1,8 @@
-import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:eco_clean_mobile_app/wrapper.dart';
 import 'firebase_options.dart';
-
-
 
 void main() async{
   runApp(MyApp());
