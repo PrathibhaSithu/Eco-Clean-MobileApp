13516cbc6eb75fc5130f3627b326f4260562b28c
PrathibhaSithu <sithuminiprathiba@gmail.com>
2024-03-29 20:24:55 +0530

    Account settings

---
+++ eco_clean_mobile_app/lib/user/xaccountsetting.dart
@@ -0,0 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:ionicons/ionicons.dart';
+
+class AccountSettings extends StatefulWidget {
+  const AccountSettings({super.key});
+
+  @override
+  State<AccountSettings> createState() => _AccountSettingsState();
+}
+
+class _AccountSettingsState extends State<AccountSettings> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
