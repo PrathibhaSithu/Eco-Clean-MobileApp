1b90faae45fbee9a7a1d212c807fc9d05457c608
PrathibhaSithu <sithuminiprathiba@gmail.com>
2024-03-29 20:06:39 +0530

    Update usermorelist.dart

--- eco_clean_mobile_app/lib/user/usermorelist.dart
+++ eco_clean_mobile_app/lib/user/usermorelist.dart
@@ -22,118 +22,113 @@ class _UserMoreListState extends State<UserMoreList> {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(onPressed: (){},
-            icon: const Icon(Ionicons.chevron_back_outline),
+          icon: const Icon(Ionicons.chevron_back_outline),
         ),
         leadingWidth: 60,
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text("Settings",
-            style: TextStyle(
-                fontSize: 36,
-                fontWeight: FontWeight.bold
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Settings",
+              style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold
+              ),
             ),
-          ),
-          const SizedBox(height: 40),
-          Text("Account",
-            style: TextStyle(
+            const SizedBox(height: 40),
+            Text("Account",
+              style: TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.w500,
+              ),
             ),
-          ),
-          Container(
-            width: double.infinity,
-            child: Row(
-              children: [
-                Image.asset("assets/imges/user.png",
-                  height: 50,
-                ),
-                const SizedBox(width: 20),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('${user!.email}',
-                    style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.w500,
-                    )),
-                    const SizedBox(height: 10),
-                    Text("User",
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Color(0xFF418E3C),
-                          fontWeight: FontWeight.w500,
-                        )),
-                  ],),
-                const Spacer(),
-                Container(
-                  width: 60,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    color: Color(0xFFC4E8C2),
-                    borderRadius: BorderRadius.circular(20),
+            Container(
+              width: double.infinity,
+              child: Row(
+                children: [
+                  Image.asset("assets/imges/user.png",
+                    height: 60,
                   ),
-                  child: const Icon(
-                    Ionicons.chevron_forward_outline,
-                  ),
-                )
-              ],
+                  const SizedBox(width: 20),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('${user!.email}',
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w500,
+                          )),
+                      const SizedBox(height: 10),
+                      Text("User",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Color(0xFF418E3C),
+                            fontWeight: FontWeight.w500,
+                          )),
+                    ],),
+                  const Spacer(),
+                  Container(
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFC4E8C2),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: const Icon(
+                      Ionicons.chevron_forward_outline,
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-          const SizedBox(height: 40),
-          Text("More",
-            style: TextStyle(
+            const SizedBox(height: 30),
+            Text("More",
+              style: TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.w500,
+              ),
             ),
-          ),
-          const SizedBox(height: 20),
-          Container(
-            width: double.infinity,
-            child: Row(
-              children: [
-                Image.asset("assets/imges/privacy.png",
-                  height: 50,
-                ),
-                const SizedBox(width: 20),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("Privacy",
-                        style: TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.w500,
-                        )),
-                    const SizedBox(height: 10),
-                    Text("Privacy Policy",
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Color(0xFF418E3C),
-                          fontWeight: FontWeight.w500,
-                        )),
-                  ],),
-                const Spacer(),
-                Container(
-                  width: 60,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    color: Color(0xFFC4E8C2),
-                    borderRadius: BorderRadius.circular(20),
+            const SizedBox(height: 20),
+            Container(
+              width: double.infinity,
+              child: Row(
+                children: [
+                  Container(
+                    child: Image.asset(
+                      "assets/imges/location.png",
+                      height: 60,
+                    ),
                   ),
-                  child: const Icon(
-                    Ionicons.chevron_forward_outline,
-                  ),
-                )
-              ],
+                  const SizedBox(width: 30),
+                  const Text("Map",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w500,
+                      )),
+                  const Spacer(),
+                  Text("Eng"),
+                  Container(
+                    width: 40,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFC4E8C2),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: const Icon(
+                      Ionicons.chevron_forward_outline,
+                    ),
+                  )
+                ],
+
+              ),
             ),
-          ),
-        ],),
+          ],),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: (()=> signout()),
         child: const Icon(Icons.login_rounded),
       ),
     );
-  }}
-
-// Text('${user!.email}'),
+  }
+}
