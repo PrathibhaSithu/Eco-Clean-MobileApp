d9ca1a325fe4eadee79bf78f36cc1d962368e973
PrathibhaSithu <sithuminiprathiba@gmail.com>
2024-03-29 18:39:48 +0530

    setting page design user account

--- eco_clean_mobile_app/lib/user/usermorelist.dart
+++ eco_clean_mobile_app/lib/user/usermorelist.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:ionicons/ionicons.dart';
 
 class UserMoreList extends StatefulWidget {
   const UserMoreList({super.key});
@@ -19,16 +20,55 @@ class _UserMoreListState extends State<UserMoreList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: SingleChildScrollView(
-        child: Text('Shopping'),
+      appBar: AppBar(
+        leading: IconButton(onPressed: (){},
+            icon: const Icon(Ionicons.chevron_back_outline),
+        ),
+        leadingWidth: 60,
       ),
-      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("Settings",
+            style: TextStyle(
+                fontSize: 36,
+                fontWeight: FontWeight.bold
+            ),
+          ),
+          const SizedBox(height: 40),
+          Text("Account",
+            style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w500,
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            child: Row(
+              children: [
+                Image.asset("assets/imges/user.png",
+                  height: 50,
+                ),
+                const SizedBox(width: 20),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('${user!.email}',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    )
+                    ),
+                  ],
+                ),//child:
+              ],
+            ),)
+        ],),
       floatingActionButton: FloatingActionButton(
         onPressed: (()=> signout()),
         child: const Icon(Icons.login_rounded),
       ),
     );
-  }
-}
+  }}
 
+// Text('${user!.email}'),
